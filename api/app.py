18afceb56f3ed496ab34bf3cf25093d16c6bfbae
ydrea <andrija.wrld@gmail.com>
2021-07-05 15:30:46 +0200

    functionality done

--- api/app.py
+++ api/app.py
@@ -7,22 +7,15 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+
+# server
+cred = []
+
 # credentials
 cred_put_args = reqparse.RequestParser()
 cred_put_args.add_argument("ip", type=str, help="host IP address")
 cred_put_args.add_argument("uname", type=str, help="User name")
 cred_put_args.add_argument("pwd", type=str, help="Password")
-
-# paramiko
-client = SSHClient()
-client.set_missing_host_key_policy(AutoAddPolicy())
-client.load_system_host_keys()
-client.connect(hostname='127.0.0.1', username='ydrea', password='lorien')
-stdin, stdout, stderr = client.exec_command('ps aux')
-psaux = f'STDOUT: {stdout.read().decode("utf8")}'
-
-# server
-cred = {}
 
 
 class SerVer(Resource):
@@ -30,6 +23,14 @@ class SerVer(Resource):
         args = cred_put_args.parse_args()
         cred = args
         return cred, 201
+# paramiko
+for it in cred:
+    client = SSHClient()
+    client.set_missing_host_key_policy(AutoAddPolicy())
+    client.load_system_host_keys()
+    client.connect(it.ip, it.uname, it.pwd)
+    stdin, stdout, stderr = client.exec_command('ps aux')
+    psaux = f'STDOUT: {stdout.read().decode("utf8")}'
 
     def get(self):
         return {'data': psaux}, 203
