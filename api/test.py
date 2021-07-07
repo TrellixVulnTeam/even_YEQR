5947ef57082e99ecffd993007833026717eb57a8
ydrea <andrija.wrld@gmail.com>
2021-07-07 04:14:06 +0200

    tials&errors

--- api/test.py
+++ api/test.py
@@ -4,8 +4,8 @@ from paramiko import SSHClient, AutoAddPolicy
 client = SSHClient()
 client.set_missing_host_key_policy(AutoAddPolicy())
 client.load_system_host_keys()
-client.connect(hostname= '127.0.0.1', username= 'ydrea', password= 'lorien')
-stdin, stdout, stderr = client.exec_command('ps aux')
+client.connect(hostname='127.0.0.1', username='ydrea', password='lorien')
+stdin, stdout, stderr = client.exec_command('ps -eo pid,pcpu,comm')
 global psaux
 psaux = f"{stdout.read().decode('utf8')}"
 # print (psaux)
